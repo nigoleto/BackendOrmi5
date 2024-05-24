9290d5fcab9f65ebc639157c032de0fcf091c60e
nigoleto <nigoleto@gmail.com>
2024-05-24 11:50:53 +0900

    240524

--- 240515/src/Main.java
+++ 240515/src/Main.java
@@ -2,9 +2,27 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Test1 test1 = new Test1();
-        System.out.println(Test1.stastr());
-        System.out.println(test1.numb());
+
+        System.out.println(solution("He11oWor1d","lloWorl",2));
+
+
+    }
+
+        public static String solution (String my_string, String overwrite_string, int s) {
+        String answer = "";
+
+        for(int i = 0; i < s; i++) {
+            answer += my_string.charAt(i);
+        }
+        for(int i = 0; i < overwrite_string.length(); i++) {
+            answer += overwrite_string.charAt(i);
+        }
+        for(int i = s + overwrite_string.length(); i < my_string.length(); i++) {
+            answer += my_string.charAt(i);
+        }
+
+        return answer;
+
 
     }
 }
